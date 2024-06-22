fc604e05a07fd3c9c1b0291304b56b044ef885b2
Rohit Kumar Sah <rohitks.com.np@gmail.com>
2024-06-22 12:42:47 +0545

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,13 +4,13 @@ self.addEventListener('install', (event) => {
       return cache.addAll([
         '/',
         '/index.html',
-        '/main.js',
-        '/ctevt-notes-manifest/manifest.json',  // Adjusted for GitHub Pages
-        'https://rohitks77.github.io/ctevt-notes-manifest/icons/icon-72x72.png',
-        'https://rohitks77.github.io/ctevt-notes-manifest/icons/icon-96x96.png',
-        'https://rohitks77.github.io/ctevt-notes-manifest/icons/icon-144x144.png',
-        'https://rohitks77.github.io/ctevt-notes-manifest/icons/icon-192x192.png',
-        'https://rohitks77.github.io/ctevt-notes-manifest/icons/icon-512x512.png',
+        'https://rohitks77.github.io/ctevt-notes-manifest/main.js',
+        'https://rohitks77.github.io/ctevt-notes-manifest/manifest.json',  // Adjusted for GitHub Pages
+        'https://rohitks77.github.io/rohitks77-manifest/icons/icon-72x72.png',
+        'https://rohitks77.github.io/rohitks77-manifest/icons/icon-96x96.png',
+        'https://rohitks77.github.io/rohitks77-manifest/icons/icon-144x144.png',
+        'https://rohitks77.github.io/rohitks77-manifest/icons/icon-192x192.png',
+        'https://rohitks77.github.io/rohitks77-manifest/icons/icon-512x512.png',
       ]);
     })
   );
