234a6b258cf1ab0d6b9172459d77f809ebc17375
Rohit Kumar Sah <rohitks.com.np@gmail.com>
2024-06-22 12:25:52 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+// main.js
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }, (err) => {
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
+
 let deferredPrompt;
 const installButton = document.getElementById('install-button');
 
