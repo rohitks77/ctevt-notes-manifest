2114799a5c92eeb61804763048a4dce643c99f89
Rohit Kumar Sah <rohitks.com.np@gmail.com>
2024-06-22 12:37:22 +0545

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,17 @@
-// service-worker.js
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('v1').then((cache) => {
       return cache.addAll([
         '/',
         '/index.html',
-        '/styles.css',
         '/main.js',
+        '/ctevt-notes-manifest/manifest.json',  // Adjusted for GitHub Pages
+        'https://rohitks77.github.io/ctevt-notes-manifest/icons/icon-72x72.png',
+        'https://rohitks77.github.io/ctevt-notes-manifest/icons/icon-96x96.png',
+        'https://rohitks77.github.io/ctevt-notes-manifest/icons/icon-144x144.png',
+        'https://rohitks77.github.io/ctevt-notes-manifest/icons/icon-192x192.png',
+        'https://rohitks77.github.io/ctevt-notes-manifest/icons/icon-512x512.png',
       ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
     })
   );
 });
