f98a8292dc478d584b2e3e6f7dfdf66148b74a9a
Rohit Kumar Sah <rohitks.com.np@gmail.com>
2024-06-22 12:25:32 +0545

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,20 @@
-self.addEventListener('install', event => {
+// service-worker.js
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('v1').then(cache => {
+    caches.open('v1').then((cache) => {
       return cache.addAll([
         '/',
         '/index.html',
         '/styles.css',
-        '/main.js'
+        '/main.js',
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
